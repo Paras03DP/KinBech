2e2b084da657bd687aa575deb060f98ede963697
Parasayer13 <paras.ayer13@gmail.com>
2024-01-10 21:07:57 +0545

    Profile page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import ListingItem from '../components/ListingItem';
 
 export default function Home() {
