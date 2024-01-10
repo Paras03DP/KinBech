909166b28caf4e15fd61ef901fff3097564dd8dc
Parasayer13 <paras.ayer13@gmail.com>
2024-01-11 02:46:14 +0545

    firebase with google login and signup

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKsVgXgb7nnDnpDQxRTJHtBEoWdDWBw7k",
+  authDomain: "kinbech-c76fd.firebaseapp.com",
+  projectId: "kinbech-c76fd",
+  storageBucket: "kinbech-c76fd.appspot.com",
+  messagingSenderId: "1060734544290",
+  appId: "1:1060734544290:web:a6afac5e24255f4e11eafb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
